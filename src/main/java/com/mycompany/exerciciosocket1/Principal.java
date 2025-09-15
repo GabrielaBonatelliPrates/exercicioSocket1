cff15be09b291d2f961b73609f2a1d5fcccf3965
GabrielaBonatelliPrates <gabriela.bonatelli06@gmail.com>
2025-09-15 09:32:35 -0300

    projeto Socket feito em aula

---
+++ src/main/java/com/mycompany/exerciciosocket1/Principal.java
@@ -0,0 +1,30 @@
+package com.mycompany.exerciciosocket1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        String endereco = JOptionPane.showInputDialog(null, "Digite o endereço que deseja consultar:");
+        int porta = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a porta que deseja consultar:"));
+        
+        try {
+            Socket sock = new Socket(endereco, porta);
+            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            String linha = "";
+            out.println("GET / HTTP/1.0\n");
+            while ((linha = in.readLine()) != null) {
+                System.out.println("echo: " + linha);
+            }
+        } catch (IOException e) {
+            System.err.println("Problemas de IO");
+        }
+    }
+
+}
